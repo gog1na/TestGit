c74fd7eb3089790e2cecca6fe2b7f953718cad3f
gogina <goginashvili.giorgi.gg@gmail.com>
2023-12-08 10:53:13 +0400

    Initial commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Giorgi Goginashvili on 12/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
